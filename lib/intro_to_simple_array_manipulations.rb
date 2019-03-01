4c8d090817cfaa9b4e729ef4c214e94b0606852e
David Thayer <dthayer16@gmail.com>
2019-03-01 20:00:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,12 +1,10 @@
 def using_push (array, string)
   array.push (string)
-  array
-end
+  end
 
 def using_unshift (array, string)
   array.unshift (string)
-  array
-end
+  end
 
 def using_pop (array)
   array.pop
